c9654e0eb8c0ff0453be9c13cfdb91053b7de82f
NISHIMWE RUKUNDO Prosper <prosper.rk1@gmail.com>
2025-01-20 06:40:14 +0200

    --amend

--- src/app/(main)/accounts/[id]/page.tsx
+++ src/app/(main)/accounts/[id]/page.tsx
@@ -1,7 +1,7 @@
 import AccountDetails from "@/components/account-details";
 import AccountTransactions from "@/components/account-transactions";
 
-export default async function AccountDetailsPage({ params }: { params: { id: string } }) {
+export default function AccountDetailsPage({ params }: { params: { id: string } }) {
     return(
         <div className="text-gray-600">
             <AccountDetails accountId={params.id}/>
