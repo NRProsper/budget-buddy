5d0dabab200d308821d58d84cfae22024d226096
NISHIMWE RUKUNDO Prosper <prosper.rk1@gmail.com>
2025-01-20 06:46:55 +0200

    --amend

--- src/app/(main)/accounts/[id]/page.tsx
+++ src/app/(main)/accounts/[id]/page.tsx
@@ -1,11 +1,18 @@
 import AccountDetails from "@/components/account-details";
 import AccountTransactions from "@/components/account-transactions";
 
-export default function AccountDetailsPage({ params }: { params: { id: string } }) {
+type PageProps = {
+    params: Promise<{
+        [x: string]: string; id: string
+    }>
+}
+
+export default async function AccountDetailsPage(params: PageProps) {
+    const pgParams = await params;
     return(
         <div className="text-gray-600">
-            <AccountDetails accountId={params.id}/>
-            <AccountTransactions accountId={params.id} />
+            <AccountDetails accountId={(await pgParams.params).id}/>
+            <AccountTransactions accountId={(await pgParams.params).id} />
         </div>
     )
 }
