b14617d19a427441b8abdb53d01b3eac85f4e6ad
NISHIMWE RUKUNDO Prosper <prosper.rk1@gmail.com>
2025-01-20 07:36:39 +0200

    --amend

--- src/components/with-auth.tsx
+++ src/components/with-auth.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, {useEffect} from "react";
+import React, {JSX, useEffect} from "react";
 import {useAuth} from "@/context/AuthContext";
 import {useRouter} from "next/navigation";
 
